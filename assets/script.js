16ea21ca5afd4219dd9b78de1d0037db7699d186
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-02-05 12:22:17 -0500

    color coded each timeblock

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,29 @@
 var currentDate = moment().format('MMMM Do YYYY, h:mm a');
 var now = document.getElementById('currentDay');
-now.innerHTML = currentDate;  
+now.innerHTML = currentDate;
+
+var taskAreaTask = document.getElementsByClassName('taskAreaTask');
+var currentHour = parseInt(moment().format('hh'));
+console.log(currentHour)
+for (var element of taskAreaTask) {
+    console.log(element)
+    var taskHour = parseInt(element.getAttribute('data-hour'));
+
+    if (taskHour < currentHour) {
+        element.classList.add('past')
+    }
+    else if (taskHour == currentHour) {
+        element.classList.add('present')
+    }
+    else {
+        element.classList.add('future')
+    }
+}
+
+
+
+
+
+// console.log(taskHour)
+// console.log({taskAreaTask});
+// console.log(currentHour);
